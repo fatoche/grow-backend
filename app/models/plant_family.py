8c6196030fecb68d0fe02f270c9d5718ac73a35e
Clara Scherer <clara@modugen.de>
2025-07-21 12:41:10 +0200

    Fix conversion from mongo document
    id of mongoengine Document is ObjectId not string

--- app/models/plant_family.py
+++ app/models/plant_family.py
@@ -1,3 +1,4 @@
+from typing import Any
 from pydantic import BaseModel
 
 
@@ -12,6 +13,11 @@ class PlantFamily(PlantFamilyBase):
 
     class Config:
         from_attributes = True
+
+    @classmethod
+    def model_validate(cls, obj: Any, **kwargs) -> "PlantFamily":
+        obj.id = str(obj.id)
+        return super().model_validate(obj, **kwargs)
 
 
 class PlantFamilyCreate(PlantFamilyBase):
